e440918cb431008f90334275ad44e7d45890f327
Aditya Partap Singh <adityapartapsingh92@gmail.com>
2026-07-13 04:37:30 +0530

    Fix Prisma ESM import for Render

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require("@prisma/client");
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 /**
